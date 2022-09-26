8eceb9a3c9663f5999af51a9c3ed89b2d6ae0fb3
liitlelenim <jakubblaszczyk20@gmail.com>
2022-09-26 21:57:02 +0200

    create data and person model

---
+++ PersonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ItransitionTask6;
+
+public class PersonModel
+{
+    public int Index { get; set; }
+    public int RandomIdentifier { get; set; }
+    public string FullName { get; set; } = String.Empty;
+    public string Address { get; set; } = String.Empty;
+    public string PhoneNumber { get; set; } = String.Empty;
+ }
+
+public enum Region
+{
+    Poland,
+    Germany,
+    UnitedStates
+}
